c0da0cc9da66ca6373c13d55fede7061e848e6be
beechnut <cloydster@gmail.com>
2016-11-12 12:26:42 -0500

    Improve subdomain constraint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_version'
 
 Rails.application.routes.draw do
 
-  namespace :api, constraints: { subdomain: 'api' }, path: '' do
+  namespace :api, constraints: lambda { |req| /^api/.match(req.host) }, path: '' do
     api_version APIVersion.v(1, default: true) do
       jsonapi_resources :organizations, only: [:index, :show]
     end
